ff753308b7c7db8d002c47a8e9072232c4ee278c
Bella Ott <bella.c.ott.04@gmail.com>
2026-04-07 17:49:04 -0500

    added app boxes to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import calculator from './assets/keys.png'
+import timer from './assets/timer.png'
+import notes from './assets/notes.png'
+import screen from './assets/monitor-screen.png'
+import chatbot from './assets/chatbot.png'
 import './App.css'
+import AppBox from './components/app-box/app-box.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,107 +13,51 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <section id="row">
+          <div>
+            <h1 style={{ paddingRight: '20px' }}>Productiv</h1>
+          </div>
+          <AppBox
+            title="Calculator"
+            label="Open calculator"
+            icon={
+              <img src={calculator} alt="Calculator" style={{ width: '90%', height: '90%' }} />
+            }
+          />
+          <AppBox
+            title="Timer"
+            label="Set timer"
+            icon={
+              <img src={timer} alt="Timer" style={{ width: '90%', height: '90%' }} />
+            }
+          />
+        </section>
+        <section id="row">
+          <AppBox
+            title="Notes"
+            label="Take notes"
+            icon={
+              <img src={notes} alt="Notes" style={{ width: '90%', height: '90%' }} />
+            }
+          />
+          <AppBox
+            title="Screen Recorder"
+            label="Record screen"
+            icon={
+              <img src={screen} alt="Screen Recorder" style={{ width: '90%', height: '90%' }} />
+            }
+          />
+          <AppBox
+            title="Google Gemini Chatbot"
+            label="Chat with Gemini"
+            icon={
+              <img src={chatbot} alt="Google Gemini Chatbot" style={{ width: '90%', height: '90%' }} />
+            }
+          />
+        </section>
       </section>
 
       <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
       <div className="ticks"></div>
       <section id="spacer"></section>
